1c5b698c55dcf98fae93b6e0bf86261f01d08bfc
Mark Anthony Aguirre <153790909+Maarrrkkkkk@users.noreply.github.com>
2025-06-29 22:00:39 +0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -88,10 +88,10 @@ export default function Page() {
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
               <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                My Projects
+             Featured Projects
               </div>
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Check out my latest work
+              <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl">
+             Academic Projects & Professional Works
               </h2>
               <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 I&apos;ve worked on a variety of projects, from simple websites
